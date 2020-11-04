ac796f24e83835bd92e078821c1a36917b3f2480
Nguyendiu <nguyendiupro15@gmail.com>
2020-11-04 21:40:16 +0700

    JS-bai1

--- index.js
+++ index.js
@@ -1,64 +1,15 @@
-document.getElementById("myInput").focus();
-document.addEventListener("keypress", (e) => {
-  if (e.charCode === 13) {
-    newElement();
-  }
-});
- let myNodelist = document.getElementsByTagName("LI");
-  var i;
-  for (i = 0; i < myNodelist.length; i++) {
-    let span = document.createElement("SPAN");
-    let txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    myNodelist[i].appendChild(span);
-  }
-  
-  // Click on a close button to hide the current list item
-  let close = document.getElementsByClassName("close");
-  var i;
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
-  }
-  
-  // Add a "checked" symbol when clicking on a list item
-  var list = document.querySelector('ul');
-  list.addEventListener('click', function(ev) {
-    if (ev.target.tagName === 'LI') {
-      ev.target.classList.toggle('checked');
-    }
-  }, false);
-  
-  // Create a new list item when clicking on the "Add" button
-  function newElement() {
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("myInput").value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if (inputValue === '') {
-      alert("You must write something!");
-    } else {
-      document.getElementById("myUL").appendChild(li);
-    }
-    document.getElementById("myInput").value = "";
-  
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
-  
-    for (i = 0; i < close.length; i++) {
-      close[i].onclick = function() {
-        var div = this.parentElement;
-        div.style.display = "none";
-      }
-    }
-    // const content = []
-    // content.push(document.getElementById("myUL").appendChild(li))
-    // console.log(content)
-  }
-  
+let str = prompt('nhap chuoi vao')
+const nearest = (arr = [], el) => arr.reduce((acc, val) => Math.min(acc,
+Math.abs(val - el)), Infinity);
+const vowelNearestDistance = (str = '') => {
+   const s = str.toLowerCase();
+   const vowelIndex = [];
+   for(let i = 0; i < s.length; i++){
+      if(s[i] === 'a' || s[i] === 'e' || s[i] === 'i' || s[i] === 'o' ||
+      s[i] === 'u'){
+         vowelIndex.push(i);
+      };
+   };
+   return s.split('').map((el, ind) => nearest(vowelIndex, ind));
+};
+console.log(vowelNearestDistance(str));
